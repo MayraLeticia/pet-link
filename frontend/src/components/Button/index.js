11836df311470a5b37360e7a75462ee3a853a481
MayraLeticia <mayraleticia03@gmail.com>
2024-09-17 23:13:26 -0300

    :bricks:ci: Integrando frontend e backend de login/registro

--- frontend/src/components/Button/index.js
+++ frontend/src/components/Button/index.js
@@ -1,7 +1,7 @@
-const Button = ({ width, heith, color, border, icon, name, onClick }) => {
+const Button = ({ width, heigth, color, border, icon, name, onClick }) => {
 
     return (
-        <button className={`${width} ${heith} rounded-[15px] ${color} border ${border} cursor-pointer`} onClick={onClick}>
+        <button className={`${width} ${heigth} rounded-[15px] ${color} border ${border} cursor-pointer flex justify-center items-center`} onClick={onClick}>
             <span className="text-2xl font-semibold text-left text-white">{name}</span>
             {icon && <img src={icon} alt="icon" />}
         </button>
