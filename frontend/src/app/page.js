472b91b95a929690aa18fdf1e417e9de5505f3be
MayraLeticia <mayraleticia03@gmail.com>
2024-10-17 18:42:32 -0300

    :sparkles: feat: Landing/Home page

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -2,13 +2,14 @@ import Image from "next/image";
 import Register from "../pages/register";
 import Login from "../pages/login";
 import Profile from "../pages/profile";
+import Home from "../pages/home";
 
 
-export default function Home() {
+export default function App() {
   return (
     <main className="flex h-screen flex-row items-center justify-center w-screen">
     
-    <Profile
+    <Home
     />
       
     </main>
