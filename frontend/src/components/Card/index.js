81ac645a151f4dd3d9fd4a85a377da6d9ae6ec66
MayraLeticia <mayraleticia03@gmail.com>
2025-03-13 18:47:58 -0300

    :broom:concertando funcionamento da tela inicial e cads

--- frontend/src/components/Card/index.js
+++ frontend/src/components/Card/index.js
@@ -11,25 +11,25 @@ const Card = ({ profilePhoto, name, location, onClick }) => {
                 />
                 
                 
-                <div className='w-1/2 h-auto gap-5 flex relative left-40 -top-40'>
-                    <div className='w-6 h-6 flex items-center justify-center rounded-full bg-slate-950 bg-opacity-60'>
-                        <img src="heart.png" className="w-[19px] h-[19px] object-cover" />
+                <div className='w-1/2 h-auto gap-5 flex relative left-36 -top-40'>
+                    <div className='w-7 h-7 flex items-center justify-center rounded-full bg-slate-700 bg-opacity-60'>
+                        <img src="icons/Heart.png" className="w-[19px] h-[19px] object-cover" />
                     </div>
-                    <div className='w-6 h-6 flex items-center justify-center rounded-full bg-slate-950 bg-opacity-60'>
-                        <img src="dislike.png" className="w-[19px] h-[19px] object-cover" />
+                    <div className='w-7 h-7 flex items-center justify-center rounded-full bg-slate-700 bg-opacity-60'>
+                        <img src="icons/Dislike.png" className="w-[19px] h-[19px] object-cover" />
                     </div>
                 </div>
                 
             </div>
             
             <div className="w-full h-1/4 rounded-r-lg p-2">
-                <p className="text-sm font-medium text-left text-black">{name}</p>
-                <p className="text-xs font-normal text-left text-slate-950 opacity-25">
+                <p className="text-sm font-bold text-left text-black">{name}</p>
+                <p className="text-xs font-medium text-left text-slate-950 opacity-40">
                     {location}
                 </p>
                     
-                <div className='w-6 h-6 flex items-center justify-center rounded-full bg-slate-950 bg-opacity-60 relative left-48 -top-7'>
-                    <img src="chat-bubbles-with-ellipsis.png" className="w-[19px] h-[19px] object-cover" />
+                <div className='w-7 h-7 flex items-center justify-center rounded-full bg-slate-700 bg-opacity-60 relative left-48 -top-7'>
+                    <img src="icons/chat-bubbles-with-ellipsis.png" className="w-[19px] h-[19px] object-cover" />
                 </div>
 
             </div>
