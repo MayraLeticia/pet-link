cb67f6b389d38540f76efed40a95ad6b836f096d
MayraLeticia <mayraleticia03@gmail.com>
2025-03-18 20:01:20 -0300

    :bug:fix: definindo rota /chat

---
+++ frontend/src/pages/chat.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const Chat = () => {
+    
+    return (
+        <div className="w-screen">
+            
+        </div>
+    );
+};
+
+export default Chat;
